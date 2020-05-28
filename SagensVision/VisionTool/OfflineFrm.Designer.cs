38e5f009992963f1e7780ebbb61a7ce9afddcb58
username <289667898@qq.com>
2020-05-28 15:09:58 +0800

    离线Frm放大禁用

--- SagensVision/VisionTool/OfflineFrm.Designer.cs
+++ SagensVision/VisionTool/OfflineFrm.Designer.cs
@@ -171,6 +171,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "OfflineFrm";
