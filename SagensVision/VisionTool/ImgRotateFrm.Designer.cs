16b74808e5f6a01a30a73cf5f6ad0551d1a58543
zzheng <289667898@qq.com>
2020-05-30 10:53:23 +0800

    ImgRotateFrm ICO

--- SagensVision/VisionTool/ImgRotateFrm.Designer.cs
+++ SagensVision/VisionTool/ImgRotateFrm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImgRotateFrm));
             this.l1 = new System.Windows.Forms.NumericUpDown();
             this.t2 = new System.Windows.Forms.NumericUpDown();
             this.r3 = new System.Windows.Forms.NumericUpDown();
@@ -45,93 +46,102 @@
             // 
             // l1
             // 
-            this.l1.Location = new System.Drawing.Point(84, 31);
+            this.l1.Location = new System.Drawing.Point(112, 39);
+            this.l1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.l1.Maximum = new decimal(new int[] {
             360,
             0,
             0,
             0});
             this.l1.Name = "l1";
-            this.l1.Size = new System.Drawing.Size(120, 21);
+            this.l1.Size = new System.Drawing.Size(160, 25);
             this.l1.TabIndex = 0;
             // 
             // t2
             // 
-            this.t2.Location = new System.Drawing.Point(84, 72);
+            this.t2.Location = new System.Drawing.Point(112, 90);
+            this.t2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.t2.Maximum = new decimal(new int[] {
             360,
             0,
             0,
             0});
             this.t2.Name = "t2";
-            this.t2.Size = new System.Drawing.Size(120, 21);
+            this.t2.Size = new System.Drawing.Size(160, 25);
             this.t2.TabIndex = 1;
             // 
             // r3
             // 
-            this.r3.Location = new System.Drawing.Point(84, 111);
+            this.r3.Location = new System.Drawing.Point(112, 139);
+            this.r3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.r3.Maximum = new decimal(new int[] {
             360,
             0,
             0,
             0});
             this.r3.Name = "r3";
-            this.r3.Size = new System.Drawing.Size(120, 21);
+            this.r3.Size = new System.Drawing.Size(160, 25);
             this.r3.TabIndex = 2;
             // 
             // d4
             // 
-            this.d4.Location = new System.Drawing.Point(84, 151);
+            this.d4.Location = new System.Drawing.Point(112, 189);
+            this.d4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.d4.Maximum = new decimal(new int[] {
             360,
             0,
             0,
             0});
             this.d4.Name = "d4";
-            this.d4.Size = new System.Drawing.Size(120, 21);
+            this.d4.Size = new System.Drawing.Size(160, 25);
             this.d4.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 33);
+            this.label1.Location = new System.Drawing.Point(64, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.Size = new System.Drawing.Size(22, 15);
             this.label1.TabIndex = 4;
             this.label1.Text = "左";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(48, 74);
+            this.label2.Location = new System.Drawing.Point(64, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.Size = new System.Drawing.Size(22, 15);
             this.label2.TabIndex = 5;
             this.label2.Text = "上";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(48, 113);
+            this.label3.Location = new System.Drawing.Point(64, 141);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.Size = new System.Drawing.Size(22, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "右";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(48, 153);
+            this.label4.Location = new System.Drawing.Point(64, 191);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.Size = new System.Drawing.Size(22, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "下";
             // 
             // btn_save
             // 
-            this.btn_save.Location = new System.Drawing.Point(267, 149);
+            this.btn_save.Location = new System.Drawing.Point(356, 186);
+            this.btn_save.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_save.Name = "btn_save";
-            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.Size = new System.Drawing.Size(100, 29);
             this.btn_save.TabIndex = 8;
             this.btn_save.Text = "保存";
             this.btn_save.UseVisualStyleBackColor = true;
@@ -139,9 +149,9 @@
             // 
             // ImgRotateFrm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 189);
+            this.ClientSize = new System.Drawing.Size(504, 236);
             this.Controls.Add(this.btn_save);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -151,6 +161,8 @@
             this.Controls.Add(this.r3);
             this.Controls.Add(this.t2);
             this.Controls.Add(this.l1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ImgRotateFrm";
             this.Text = "ImgRotateFrm";
             this.Load += new System.EventHandler(this.ImgRotateFrm_Load);
